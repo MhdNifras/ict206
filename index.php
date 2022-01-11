c9fbeee8781f87ce8cc53e9d0c96bb6bb2467e4b
nifras <nifras1998221@gmail.com>
2022-01-11 15:36:16 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+
+class StudentParent{
+            	public $stdName;
+            	public $stdRegNum;
+
+            	function __construct($name, $regnum) {
+            		$this->stdName = $name;
+            		$this->stdRegNum = $regnum;
+			  	}
+
+			  	
+            }
